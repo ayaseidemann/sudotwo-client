d48538a8b370b296a1375af3771518b5a2ddf631
Aya Seidemann <aya.seidemann@gmail.com>
2022-09-21 08:17:04 -0700

    add create and join room pages the emit to socket

--- src/pages/CreateGame/CreateGame.js
+++ src/pages/CreateGame/CreateGame.js
@@ -1,7 +1,25 @@
 import './CreateGame.scss';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
+import ShortUniqueId from 'short-unique-id';
+import { useNavigate } from 'react-router-dom';
 
-function CreateGame() {
 
+function CreateGame({ socket }) {
+
+    let navigate = useNavigate();
+
+    function joinNewRoom(event) {
+        const uid = new ShortUniqueId({ length: 5 });
+        const roomId = uid();
+        console.log('joining room: ', roomId);
+        socket.emit('join-room', roomId);
+        navigate(`/game/${roomId}`);
+    }
+    
+    return(
+        
+        <SubmitButton clickHandler={joinNewRoom} text='Create Game'/>
+    )
 }
 
 export default CreateGame;
--- src/pages/JoinGame/JoinGame.js
+++ src/pages/JoinGame/JoinGame.js
@@ -1,7 +1,31 @@
+
 import './JoinGame.scss';
+import SubmitButton from '../../components/SubmitButton/SubmitButton';
+import { useNavigate } from 'react-router-dom';
 
-function JoinGame() {
 
+
+function JoinGame({ socket }) {
+
+    let navigate = useNavigate();
+
+    const joinExistingRoom = (event) => {
+        // event.preventDefault();
+        const roomId = event.target.roomId.value;
+        console.log('joining room: ', roomId);
+        socket.emit('join-room', roomId);
+        navigate(`/game/${roomId}`);
+    }
+
+    return(
+        <form onSubmit={joinExistingRoom}>
+            <label>Room code
+                <input type='text' name='roomId'></input>
+            </label>
+            <SubmitButton text='Join Game'/>
+        </form>
+        
+    )
 }
 
 export default JoinGame;
